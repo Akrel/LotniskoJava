260ca25ea7faf6e8ef548afe09f640604e210d54
unknown <wojtekgrelewicz@gmail.com>
2020-04-02 08:52:49 +0200

    rozdzielone

---
+++ complete/src/main/java/com/example/accessingdatamysql/Flights.java
@@ -0,0 +1,49 @@
+package com.example.accessingdatamysql;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Flights {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Date data_wylotu;
+    private Date date_przylotu;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "destination")
+    private Airport destination;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "orgin")
+    private Airport orgin;
+
+    public Date getData_wylotu() {
+        return data_wylotu;
+    }
+
+    public Date getDate_przylotu() {
+        return date_przylotu;
+    }
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setData_wylotu(Date data_wylotu) {
+        this.data_wylotu = data_wylotu;
+    }
+
+    public void setDate_przylotu(Date date_przylotu) {
+        this.date_przylotu = date_przylotu;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+}
